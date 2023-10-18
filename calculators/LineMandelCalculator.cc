c7429e3454ff1d7fe02a0550c630de31fa12b85e
komca12 <komca12@gmail.com>
2023-10-18 15:04:08 +0200

    Update LineMandelCalculator.cc

--- calculators/LineMandelCalculator.cc
+++ calculators/LineMandelCalculator.cc
@@ -8,7 +8,7 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-
+#include <immintrin.h>
 #include <stdlib.h>
 
 
@@ -18,36 +18,15 @@
 LineMandelCalculator::LineMandelCalculator (unsigned matrixBaseSize, unsigned limit) :
 	BaseMandelCalculator(matrixBaseSize, limit, "LineMandelCalculator")
 {
-	data = (int *)(malloc(height * width * sizeof(int)));
-	//data= (int *)_mm_malloc(height * width * sizeof(int), height);
+	//data = (int *)(malloc(height * width * sizeof(int)));
+	data= (int *)_mm_malloc(height * width * sizeof(int), height);
 }
 
 LineMandelCalculator::~LineMandelCalculator() {
-	free(data);
-	//_mm_free(data);
+	//free(data);
+	_mm_free(data);
 	data = NULL;
 }
-
-template <typename T>
-static inline int mandelbrot(const T real,const T imag,const int limit)
-{
-	T zReal = real;
-	T zImag = imag;
-
-	int i;
-	for (i = 0; i < limit; ++i)
-	{
-		T r2 = zReal * zReal;
-		T i2 = zImag * zImag;
-
-		if (r2 + i2 > 4.0f)
-			break;
-
-		zImag = 2.0f * zReal * zImag + imag;
-		zReal = r2 - i2 + real;
-	}
-	return i;
-}//dwad
 
 int * LineMandelCalculator::calculateMandelbrot () {
 	int *pdata = data;
@@ -60,36 +39,30 @@ int * LineMandelCalculator::calculateMandelbrot () {
 	const float c_dy = dy;
 
 
+	#pragma omp simd collapse(2)
 	for (int i = 0; i < c_height; i++)
 	{
-		#pragma omp simd
-		for (int j = 0; j < c_width; j+=4)
+		//#pragma omp simd
+		for (int j = 0; j < c_width; j++)
 		{
-			float x[4];
-			float y[4];
-			int value[4];
+			float x=c_x_start + j * c_dx;
+			float y=c_y_start + i * c_dy;
+			float zReal = x;
+			float zImag = y;
 
-			// Compute four real and imaginary values
-			#pragma unroll
-			for (int k = 0; k < 4; k++)
+			int l;
+			for (l = 0; l < limit; ++l)
 			{
-				x[k] = c_x_start + (j + k) * c_dx;
-				y[k] = c_y_start + i * c_dy;
-			}
+				float r2 = zReal * zReal;
+				float i2 = zImag * zImag;
 
-			// Compute Mandelbrot values for the four points
-			#pragma unroll
-			for (int k = 0; k < 4; k++)
-			{
-				value[k] = mandelbrot(x[k], y[k], c_limit);
-			}
+				if (r2 + i2 > 4.0f)
+					break;
 
-			// Store the Mandelbrot values
-			#pragma unroll
-			for (int k = 0; k < 4; k++)
-			{
-				pdata[i * c_width + j + k] = value[k];
+				zImag = 2.0f * zReal * zImag + y;
+				zReal = r2 - i2 + x;
 			}
+			pdata[i * c_width + j ] = l;
 		}
 	}
 	return data;
